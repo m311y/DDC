dc1647c9641287980e02e837fda70f2341523640
Melissa Hardwick <melissaohardwick@gmail.com>
2019-03-20 18:47:46 -0400

    added slides with scrolling and dummy data

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import './Carousel.css';
 
+import data from './data.json';
+import Slide from './Slide';
+
 class Carousel extends Component {
+    renderSlides() {
+        return data.map((state)=>{
+          return( 
+          <Slide
+             name={state.name}
+             key={state.abbreviation}
+            />
+        );
+        })
+    }
     render () {
+        
         return (
             <div className="carousel-container">
                 <button className="carousel-nav carousel-left-nav"></button>
-               <div className="carousel-viewport"></div>
+               <div className="carousel-viewport">
+                   {this.renderSlides()}
+               </div>
                 <button className="carousel-nav carousel-right-nav"></button>
             </div>
         );
